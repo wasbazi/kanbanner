108d130d4517e891b300b3d490aa845a440dc1ba
Avi W-H <wasbazi@gmail.com>
2014-09-07 21:05:25 -0600

    Update story service to use `stories` in url

--- public/javascript/angular/story/StoryService.js
+++ public/javascript/angular/story/StoryService.js
@@ -15,7 +15,7 @@ mod.service("Story", ["$rootScope", "$http", function($rootScope, $http){
 
   function updateStory(story, state){
     console.log("story", story.state)
-    $http.post("/story/" + story.id, story).success(function(data){
+    $http.post("/stories/" + story.id, story).success(function(data){
       console.log('response', data)
       if(state.name != data.state){
         console.error("state not updated")
--- public/javascript/compiled.js
+++ public/javascript/compiled.js
@@ -95,13 +95,43 @@ mod.service("Story", ["$rootScope", "$http", function($rootScope, $http){
 
   function updateStory(story, state){
     console.log("story", story.state)
-    $http.post("/story/" + story.id, story).success(function(data){
+    $http.post("/stories/" + story.id, story).success(function(data){
       console.log('response', data)
       if(state.name != data.state){
         console.error("state not updated")
       }
     })
   }
+}])
+
+var mod = angular.module("storyList", ["story.services", "story.directives", "state.services"])
+
+mod.controller("StoryListCtrl", ["$scope", "$rootScope", "Story", "State", function($scope, $rootScope, Story, State){
+  $scope.$on("states.update", function(){
+    $scope.states = State.states
+    console.log('states.update', $scope.states)
+  })
+
+  $scope.$on("stories.update", function(){
+    $scope.stories = Story.stories
+    console.log('stories.update', $scope.stories)
+  })
+
+  $scope.$on("story.move", function(event, args){
+    var idx = null
+    var stories = $scope.stories[args.prev]
+
+    stories.forEach(function(item, i){
+      if(item.title == args.title) return idx = i
+    })
+
+    var story = stories.splice(idx, 1)[0]
+    story.state = args.state.id
+    $scope.stories[args.state.name].push(story)
+    $scope.$apply()
+
+    Story.update(story, args.state)
+  })
 }])
 
 var mod = angular.module("storyList", ["story.services", "story.directives", "state.services"])
